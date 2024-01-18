5eb31d0f3768372d0f4a4923f0c2d7fa9b5e1f87
Ngoubou <ralphngoubou@gmail.com>
2024-01-18 23:30:38 +0100

    Create repeat.py

---
+++ repeat.py
@@ -0,0 +1,9 @@
+def find(s):
+    return
+
+if __name__ == "__main__":
+    print(find("aaa")) # 1
+    print(find("abcd")) # 4
+    print(find("abcabcabcabc")) # 3
+    print(find("aybabtuaybabtu")) # 7
+    print(find("abcabca")) # 7
