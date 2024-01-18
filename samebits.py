a9f2fe5b8cc5eb2055b4a5324d19981ed06da9c8
Ngoubou <ralphngoubou@gmail.com>
2024-01-18 14:28:37 +0100

    Create same bits.py

---
+++ samebits.py
@@ -0,0 +1,8 @@
+def count(s):
+    return
+
+if __name__ == "__main__":
+    print(count("01101")) # 2
+    print(count("1111")) # 0
+    print(count("101111")) # 1
+    print(count("00001111")) # 4
