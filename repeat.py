2e943499fa5d42ea50dbecc6522112a62f1e9996
Ngoubou <ralphngoubou@gmail.com>
2024-01-19 00:13:02 +0100

    Update repeat.py

--- repeat.py
+++ repeat.py
@@ -1,9 +1,25 @@
-def find(s):
-    return
+# def find(s):
+#     return
 
-if __name__ == "__main__":
-    print(find("aaa")) # 1
-    print(find("abcd")) # 4
-    print(find("abcabcabcabc")) # 3
-    print(find("aybabtuaybabtu")) # 7
-    print(find("abcabca")) # 7
+# if __name__ == "__main__":
+#     print(find("aaa")) # 1
+#     print(find("abcd")) # 4
+#     print(find("abcabcabcabc")) # 3
+#     print(find("aybabtuaybabtu")) # 7
+#     print(find("abcabca")) # 7
+
+s = "abcabcabcabc"
+
+
+total = []
+for i in range(len(s)):
+    count = 1
+    for j in range(i+1, len(s)):
+        if s[i] == s[j]: #sourcery skip
+            pass
+        else:
+            count += 1
+    total.append(count)
+
+print(max(total))
+print(count)
