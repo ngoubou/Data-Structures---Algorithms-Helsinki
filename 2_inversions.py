fae89c096cdfe2213b745586e35564ec8f9f9e0a
Ngoubou <ralphngoubou@gmail.com>
2024-01-18 14:11:48 +0100

    Update 2_inversions.py

--- 2_inversions.py
+++ 2_inversions.py
@@ -1,18 +1,35 @@
 """
 Inversions
-https://cses.fi/dsa24k/task/3055
+https://cses.fi/dsa24k/task/3049
 """
 
-def count(a, b, c):
-    """
-    We choose the minimum price between the gummy and the chocolate. 
-    Then, we divide the money we have by the previous result.
-    """
-    return c//min(a,b)
-
+def count(t: list):
+    inv = 0
+    for i in range(len(t)):
+        for j in range(i+1,len(t)):
+            if (i < j) and t[i] > t[j]:
+                inv += 1
+    return inv
 
 if __name__ == "__main__":
-    print(count(3, 4, 11)) # 3
-    print(count(5, 1, 100)) # 100
-    print(count(2, 3, 1)) # 0
-    print(count(2, 3, 9)) # 4
+    print(count([1,3,2])) # 1
+    print(count([1])) # 0
+    print(count([4,3,2,1])) # 6
+    print(count([1,8,2,7,3,6,4,5])) # 12
+
+# inv = 0
+# new_var = [1,8,2,7,3,6,4,5]
+# # for indices,values in enumerate(new_var):
+# #     try:
+# #         if (indices < indices + 1) and new_var[indices] > new_var[indices + 1]:
+# #             inv += 1
+# #     except IndexError:
+# #         pass
+
+# for i in range(len(new_var)):
+#     #print(f"valeur de i: {i}")
+#     for j in range(i+1,len(new_var)):
+#         if (i < j) and new_var[i] > new_var[j]:
+#             inv += 1
+#         #print(f"valeur de j: {j}")
+# print(inv)
