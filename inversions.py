08f515549cb28a91a1ef6dd722407209115df014
Ngoubou <ralphngoubou@gmail.com>
2024-01-18 13:38:57 +0100

    Create inversions.py

---
+++ inversions.py
@@ -0,0 +1,18 @@
+"""
+Candies
+https://cses.fi/dsa24k/task/3055
+"""
+
+def count(a, b, c):
+    """
+    We choose the minimum price between the gummy and the chocolate. 
+    Then, we divide the money we have by the previous result.
+    """
+    return c//min(a,b)
+
+
+if __name__ == "__main__":
+    print(count(3, 4, 11)) # 3
+    print(count(5, 1, 100)) # 100
+    print(count(2, 3, 1)) # 0
+    print(count(2, 3, 9)) # 4
