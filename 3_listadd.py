51af84ebec1fe5299688e46c0c53ab2a9365c4e2
Ngoubou <ralphngoubou@gmail.com>
2024-01-27 22:21:48 +0100

    Update 3_listadd.py

--- 3_listadd.py
+++ 3_listadd.py
@@ -8,3 +8,5 @@ if __name__ == "__main__":
     print(create([1,2,3],1)) # [2,3,4]
     print(create([1,1,1,1,1],4)) # [5,5,5,5,5]
     print(create([0],10**9)) # [1000000000]
+
+# Could have been implemented with list comprehension
