65846e499154ce7b872c74ea40a301813fdffd98
Ngoubou <ralphngoubou@gmail.com>
2024-01-29 17:14:25 +0100

    Update circlegame.py

--- circlegame.py
+++ circlegame.py
@@ -6,7 +6,13 @@
 #         order.append(circle[i])
 #         circle.pop(i)
 #         i += 1
-#     order.extend(circle)
+#     else:
+#         for index in range(len(circle)):
+#             if index == len(circle):
+#                 order.extend(circle)
+#                 break
+#             order.append(circle[index])
+#             circle.pop(index)
 #     return order
 
 # if __name__ == "__main__":
@@ -15,20 +21,10 @@
 #     print(create(7)) # [2,4,6,1,5,3,7]
     
     
-n = 7
+n = 4
 
 order = [] #sourcery skip
 circle = list(range(1,n+1))
-# for i in range(len(circle)):
-#     print(circle[i+1])
-#cir = circle.copy()
-# for i in range(len(cir)):
-#     try:
-#         order.append(circle[i+1]) # ajouter cet élément à la liste order
-#         circle.pop(i+1)
-#     except IndexError:
-#         pass
-# #print(circle[1:])
 
 i = 1
 while i < len(circle):
@@ -38,7 +34,7 @@ while i < len(circle):
 
 else:
     #order.extend(circle)
-    print(order)
+    #print(order)
     for index in range(len(circle)):
         if index == len(circle):
             order.extend(circle)
@@ -46,7 +42,4 @@ else:
         order.append(circle[index])
         circle.pop(index)
         
-    print(order)
-# for i in range(len(circle)):
-#     if i < len(circle):
-#         order.append(circle[i+1])
+print(order)
